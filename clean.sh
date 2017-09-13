754831eeb80b5faada4052a610f956e2d2d59419
ggpwnkthx <ibjessup@gmail.com>
2017-09-13 15:21:41 -0400

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,11 +1,13 @@
 echo "mlx4_core" >> /etc/modules
 echo "mlx4_ib" >> /etc/modules
+echo "rdma_ucm" >> /etc/modules
 echo "ib_umad" >> /etc/modules
 echo "ib_uverbs" >> /etc/modules
 echo "ib_ipoib" >> /etc/modules
 
 modprobe mlx4_core
 modprobe mlx4_ib
+modprobe rdma_ucm
 modprobe ib_umad
 modprobe ib_uverbs
 modprobe ib_ipoib
@@ -16,3 +18,7 @@ update-rc.d -f opensm remove
 update-rc.d opensm defaults
 update-rc.d opensm enable
 service opensm restart
+
+# Add the following to IB interfaces in /etc/network/interfaces
+#pre-up echo connected > /sys/class/net/ib0/mode
+#mtu 65520
